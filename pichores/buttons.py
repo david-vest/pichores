0cadc46fe07b0ba12fb5cfb5cebee4dc8470fcb7
bestvestinthewest <bestvestinthewest@gmail.com>
2021-11-09 08:29:02 -0600

    Setup buttons

--- pichores/buttons.py
+++ pichores/buttons.py
@@ -2,27 +2,29 @@ from digitalio import DigitalInOut, Direction, Pull
 import board
 import RPi.GPIO as GPIO
 
+
 class BonnetInput:
 
     def setup_buttons(self):
-       buttons = {
-           "L": 27,
-           "R": 23,
-           "U": 17,
-           "D": 22,
-           "C": 4,
-           "A": 5,
-           "B": 6
-       } 
+        buttons = {
+            "L": 27,
+            "R": 23,
+            "U": 17,
+            "D": 22,
+            "C": 4,
+            "A": 5,
+            "B": 6
+        }
 
-       GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
+        for key in buttons:
+            GPIO.setup(buttons[key], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-       for key in buttons:
-           GPIO.setup(buttons[key]), GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        return buttons
 
     def __init__(self):
-        setup_buttons()
+        self.buttons = self.setup_buttons()
 
     def register_callback(self, button_name, callback_func):
-        GPIO.add_event_detect(self.buttons[button_name], GPIO.FALLING, callback=callback_func, bouncetime=800)
-        
+        GPIO.add_event_detect(
+            self.buttons[button_name], GPIO.FALLING, callback=callback_func, bouncetime=800)
--- pichores/pichores.py
+++ pichores/pichores.py
@@ -1,26 +1,54 @@
 import asyncio
-import buttons
+import os
 import board
+import importlib
 import busio
+from buttons import BonnetInput
+import buttons
 from digitalio import DigitalInOut, Direction, Pull
 from PIL import Image, ImageDraw
 import adafruit_ssd1306
+from ui.windowcontroller import WindowController
+from windows import *
 
 
 def main():
     loop = asyncio.get_event_loop()
 
     i2c = busio.I2C(board.SCL, board.SDA)
-    oled = adafruit_ssd1306.I2C(128, 64, i2c)
+    oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
     
     buttons = BonnetInput()
 
+    window_controller = WindowController(loop, oled)
+
+    # for file_name in os.listdir("windows"):
+    #     if file_name.endswith(".py"):
+    #         windowid = file_name[:-3]
+    #         module = importlib.import_module(f"windows.{windowid}")
+    #         windowclass = getattr(module, windowid.capitalize())
+    #         window = windowclass(window_controller)
+    #         window_controller.add_window(windowid, window)
     
+    # window_controller.set_window("start")
 
+    oled.fill(0)
+    oled.show()
 
+    width = oled.width
+    height = oled.height
 
+    image = Image.new("1", (width, height))
+    draw = ImageDraw.Draw(image)
+    
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
+    buttons.register_callback("A", a_pressed(draw))
+    
+    loop.run_forever()
 
+def a_pressed(draw):
+    draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=1)
 
 if __name__ == '__main__':
     main()
